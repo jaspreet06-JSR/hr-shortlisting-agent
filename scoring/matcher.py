d1f78cadc95210e5860718d631edcfc5f582d7e4
Jaspreet singh <jaspreetsingh@Jaspreets-MacBook-Air.local>
2026-05-09 15:56:59 +0530

    Fixed matcher NoneType bug

--- scoring/matcher.py
+++ scoring/matcher.py
@@ -1,12 +1,27 @@
-def match_skills(text, skills_db):
+def match_skills(jd_skills, resume_text):
 
-    detected_skills = []
+    if resume_text is None:
+        resume_text = ""
 
-    text = text.lower()
+    resume_text = str(resume_text).lower()
 
-    for skill in skills_db:
+    matched_skills = []
+    missing_skills = []
 
-        if skill.lower() in text:
-            detected_skills.append(skill)
+    for skill in jd_skills:
 
-    return detected_skills
+        if skill.lower() in resume_text:
+            matched_skills.append(skill)
+
+        else:
+            missing_skills.append(skill)
+
+    total_skills = len(jd_skills)
+
+    if total_skills == 0:
+        score = 0
+
+    else:
+        score = (len(matched_skills) / total_skills) * 100
+
+    return matched_skills, missing_skills, score
